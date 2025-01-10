b2ee7e61c8b18608a69ded98d0303fafb52c3196
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-01-10 18:54:55 +0900

    bump Next.js to v15 and ESLint to v9

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import withSerwistInit from '@serwist/next';
 
-/** @type {import('next').NextConfig} */
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
   // use something else that works, such as "service-worker/index.ts".
